675dbecd8f8acff2cd2e3b4c2d2ca84b4dd7e624
abhishekdas <dasabhshek522@gmail.com>
2024-10-11 20:56:47 +0530

    url changes

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 
 	
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{ "https://movierec-client.vercel.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
